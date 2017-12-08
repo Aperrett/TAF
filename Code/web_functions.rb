4f373856bb3d784e841772410c9d957c6aff9777
Aperrett <32389685+Aperrett@users.noreply.github.com>
2017-12-08 15:43:10 +0000

    updated radio function

--- Code/web_functions.rb
+++ Code/web_functions.rb
@@ -265,9 +265,9 @@ module WebFuncs
   end # checkBoxData
 
   # Radio button function.
-  def self.radio_button(radio, value, locate, locate2 )
-    $browser.radio(:"#{locate}" => radio).wait_until_present.click
-    $browser.radio(:"#{locate}" => radio).option(:"#{locate2}" => value).set
+  def self.radio_button(radio, value, locate, locate2)
+    $browser.radio(:"#{locate}" => radio).wait_until_present
+    $browser.radio(:"#{locate}" => radio, :"#{locate2}" => value).set
     $results_file.write("Radio button: #{radio} has been selected")
     $PDF.text("Radio button: #{radio} has been selected")
     $results_file.puts ''
