029cd1afc0a79dbbac95daa5da08b671707d1289
Andy <32389685+Aperrett@users.noreply.github.com>
2018-01-02 11:53:29 +0000

    Update Start_TAF_With_Selenium_Grid.sh

--- Code/Start_TAF_With_Selenium_Grid.sh
+++ Code/Start_TAF_With_Selenium_Grid.sh
@@ -8,7 +8,7 @@ build_record() {
   docker build -t taf .
 }
 
-build_record
+sudo build_record
 echo -e $(docker build -t taf .) 
 
 # Start Selenium Grid in docker.
@@ -19,7 +19,7 @@ build_selenium_grid() {
   docker-compose up -d
 }
 
-build_selenium_grid
+sudo build_selenium_grid
 echo -e $(docker-compose up -d)
 echo ""
 echo 'Open a browser to Selenium grid: http://localhost:4444/grid/console'
@@ -29,7 +29,7 @@ echo ""
 echo "Launching the TAF container into the container shell"
 echo "To execute a test type the following code below:"
 echo "ruby main.rb Tests/TS_<filename>"
-docker run --rm --network taf_selenium_grid_internal --link selenium_hub:hub -it --name taf -v "$(pwd)"/target:/app/Results:cached taf sh
+sudo docker run --rm --network taf_selenium_grid_internal --link selenium_hub:hub -it --name taf -v "$(pwd)"/target:/app/Results:cached taf sh
 
 # Exit the script.
 exit $?
