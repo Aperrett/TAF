3e1e477f1c1c0a5661344dd25fe3c8b87143b7aa
Aperrett <32389685+Aperrett@users.noreply.github.com>
2018-01-04 11:38:32 +0000

    updated for XML summary output report

--- Code/create_directories.rb
+++ Code/create_directories.rb
@@ -80,8 +80,8 @@ module CreateDirectories
     # the test suite summary is a pdf report generated will be called 'suite_summary.pdf'
     $TestSuiteSummaryPDF = abs_path_run_no_dir + '/suite_summary.pdf'
 
-    # the test suite summary is a xml report generated will be called 'suite_summary.xml'
-    $TestSuiteSummaryXML = abs_path_run_no_dir + '/suite_summary.xml'
+    # the test suite summary is a XML report generated will be called 'suite_summary.xml'
+    $TestSuiteSummaryXML = result_home + '/suite_summary.xml'
 
     # the log file name will be under the test ID directory
     $logFileName = testid_dir + '/TestLogFile.txt'
