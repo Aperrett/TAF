11456809b3aed2ed5e201b6f5cb3cac123abe514
Aperrett <32389685+Aperrett@users.noreply.github.com>
2018-08-02 17:46:19 +0100

    tidy up test_steps.rb

--- Code/test_steps.rb
+++ Code/test_steps.rb
@@ -26,32 +26,32 @@ module TestSteps
       # print the test step information
       Report.printTestStepHeader(test_file_name, testStepIndex)
       # call the appropriate method
-      test_pass = WebFuncs.open_url(value1) if runtest == false
-      Report.testPassFail(test_pass, test_file_name, testStepIndex)
+      testfunc = WebFuncs.open_url(value1) if runtest == false
+      Report.testPassFail(testfunc, test_file_name, testStepIndex)
       Report.checkFailureThreshold(test_file_name)
 
     when 'portal_login'
       # print the test step information
       Report.printTestStepHeader(test_file_name, testStepIndex)
       # call the appropriate method
-      test_pass = CustomFuncs.portal_login(value1) if runtest == false
-      Report.testPassFail(test_pass, test_file_name, testStepIndex)
+      testfunc = CustomFuncs.portal_login(value1) if runtest == false
+      Report.testPassFail(testfunc, test_file_name, testStepIndex)
       Report.checkFailureThreshold(test_file_name)
 
     when 'admin_portal_login'
       # print the test step information
       Report.printTestStepHeader(test_file_name, testStepIndex)
       # call the appropriate method
-      test_pass = CustomFuncs.portal_admin_login if runtest == false
-      Report.testPassFail(test_pass, test_file_name, testStepIndex)
+      testfunc = CustomFuncs.portal_admin_login if runtest == false
+      Report.testPassFail(testfunc, test_file_name, testStepIndex)
       Report.checkFailureThreshold(test_file_name)
 
     when 'sint_login'
       # print the test step information
       Report.printTestStepHeader(test_file_name, testStepIndex)
       # call the appropriate method
-      test_pass = CustomFuncs.sint_login if runtest == false
-      Report.testPassFail(test_pass, test_file_name, testStepIndex)
+      testfunc = CustomFuncs.sint_login if runtest == false
+      Report.testPassFail(testfunc, test_file_name, testStepIndex)
       Report.checkFailureThreshold(test_file_name)
 
     when 'ping_test'
@@ -59,16 +59,16 @@ module TestSteps
       # print the test step information
       Report.printTestStepHeader(test_file_name, testStepIndex)
       # call the appropriate method
-      test_pass = WebFuncs.ping_test(value1) if runtest == false
-      Report.testPassFail(test_pass, test_file_name, testStepIndex)
+      testfunc = WebFuncs.ping_test(value1) if runtest == false
+      Report.testPassFail(testfunc, test_file_name, testStepIndex)
       Report.checkFailureThreshold(test_file_name)
 
     when 'check_url'
       # print the test step information
       Report.printTestStepHeader(test_file_name, testStepIndex)
       # call the appropriate method
-      test_pass = WebFuncs.check_url(value1) if runtest == false
-      Report.testPassFail(test_pass, test_file_name, testStepIndex)
+      testfunc = WebFuncs.check_url(value1) if runtest == false
+      Report.testPassFail(testfunc, test_file_name, testStepIndex)
       Report.checkFailureThreshold(test_file_name)
 
     when 'write_to_editor'
@@ -77,19 +77,19 @@ module TestSteps
       Report.printTestStepHeader(test_file_name, testStepIndex)
       # call the appropriate method
       if runtest == false
-        test_pass = WebFuncs.write_to_editor(value1, locate1, value2)
+        testfunc = WebFuncs.write_to_editor(value1, locate1, value2)
       end
       # Check for test Pass / Fail
-      Report.testPassFail(test_pass, test_file_name, testStepIndex)
+      Report.testPassFail(testfunc, test_file_name, testStepIndex)
       Report.checkFailureThreshold(test_file_name)
 
     when 'capture_alert'
       # print the test step information
       Report.printTestStepHeader(test_file_name, testStepIndex)
       # call the appropriate method
-      test_pass = WebFuncs.capture_alert if runtest == false
+      testfunc = WebFuncs.capture_alert if runtest == false
       # Check for test Pass / Fail
-      Report.testPassFail(test_pass, test_file_name, testStepIndex)
+      Report.testPassFail(testfunc, test_file_name, testStepIndex)
       Report.checkFailureThreshold(test_file_name)
 
     when 'click_button'
@@ -97,9 +97,9 @@ module TestSteps
       # print the test step information
       Report.printTestStepHeader(test_file_name, testStepIndex)
       # call the appropriate method
-      test_pass = WebFuncs.click_button(value1, locate1) if runtest == false
+      testfunc = WebFuncs.click_button(value1, locate1) if runtest == false
       # Check for test Pass / Fail
-      Report.testPassFail(test_pass, test_file_name, testStepIndex)
+      Report.testPassFail(testfunc, test_file_name, testStepIndex)
       Report.checkFailureThreshold(test_file_name)
 
     when 'select_dropdown'
@@ -108,10 +108,10 @@ module TestSteps
       Report.printTestStepHeader(test_file_name, testStepIndex)
       # call the appropriate method
       if runtest == false
-        test_pass = WebFuncs.select_dropdown(value1, value2, locate1, locate2)
+        testfunc = WebFuncs.select_dropdown(value1, value2, locate1, locate2)
       end
       # Check for test Pass / Fail
-      Report.testPassFail(test_pass, test_file_name, testStepIndex)
+      Report.testPassFail(testfunc, test_file_name, testStepIndex)
       Report.checkFailureThreshold(test_file_name)
 
     when 'list_all_dropdown_values'
@@ -119,9 +119,9 @@ module TestSteps
       # print the test step information
       Report.printTestStepHeader(test_file_name, testStepIndex)
       # call the appropriate method
-      test_pass = WebFuncs.list_dropdowns(value1, locate1) if runtest == false
+      testfunc = WebFuncs.list_dropdowns(value1, locate1) if runtest == false
       # Check for test Pass / Fail
-      Report.testPassFail(test_pass, test_file_name, testStepIndex)
+      Report.testPassFail(testfunc, test_file_name, testStepIndex)
       Report.checkFailureThreshold(test_file_name)
 
     when 'write_box_data'
@@ -130,10 +130,10 @@ module TestSteps
       Report.printTestStepHeader(test_file_name, testStepIndex)
       # call the appropriate method
       if runtest == false
-        test_pass = WebFuncs.write_text(value1, value2, locate1)
+        testfunc = WebFuncs.write_text(value1, value2, locate1)
       end
       # Check for test Pass / Fail
-      Report.testPassFail(test_pass, test_file_name, testStepIndex)
+      Report.testPassFail(testfunc, test_file_name, testStepIndex)
       Report.checkFailureThreshold(test_file_name)
 
     when 'check_box_data'
@@ -142,10 +142,10 @@ module TestSteps
       Report.printTestStepHeader(test_file_name, testStepIndex)
       # call the appropriate method
       if runtest == false
-        test_pass = WebFuncs.check_boxdata(value1, value2, locate1)
+        testfunc = WebFuncs.check_boxdata(value1, value2, locate1)
       end
       # Check for test Pass / Fail
-      Report.testPassFail(test_pass, test_file_name, testStepIndex)
+      Report.testPassFail(testfunc, test_file_name, testStepIndex)
       Report.checkFailureThreshold(test_file_name)
 
     when 'check_screen_data'
@@ -153,9 +153,9 @@ module TestSteps
       # print the test step information
       Report.printTestStepHeader(test_file_name, testStepIndex)
       # call the appropriate method
-      test_pass = WebFuncs.check_screendata(value1) if runtest == false
+      testfunc = WebFuncs.check_screendata(value1) if runtest == false
       # Check for test Pass / Fail
-      Report.testPassFail(test_pass, test_file_name, testStepIndex)
+      Report.testPassFail(testfunc, test_file_name, testStepIndex)
       Report.checkFailureThreshold(test_file_name)
 
     when 'check_browser_title'
@@ -163,9 +163,9 @@ module TestSteps
       # print the test step information
       Report.printTestStepHeader(test_file_name, testStepIndex)
       # call the appropriate method
-      test_pass = WebFuncs.check_title(value1) if runtest == false
+      testfunc = WebFuncs.check_title(value1) if runtest == false
       # Check for test Pass / Fail
-      Report.testPassFail(test_pass, test_file_name, testStepIndex)
+      Report.testPassFail(testfunc, test_file_name, testStepIndex)
       Report.checkFailureThreshold(test_file_name)
 
     when 'check_log_file'
@@ -173,28 +173,28 @@ module TestSteps
       Report.printTestStepHeader(test_file_name, testStepIndex)
       # call the appropriate method
       if runtest == false
-        test_pass = WebFuncs.check_log_file(value1, value2, locate1)
+        testfunc = WebFuncs.check_log_file(value1, value2, locate1)
       end
       # Check for test Pass / Fail
-      Report.testPassFail(test_pass, test_file_name, testStepIndex)
+      Report.testPassFail(testfunc, test_file_name, testStepIndex)
       Report.checkFailureThreshold(test_file_name)
 
     when 'ipause'
       # print the test step information
       Report.printTestStepHeader(test_file_name, testStepIndex)
       # call the appropriate method
-      test_pass = WebFuncs.ipause(value1) if runtest == false
+      testfunc = WebFuncs.ipause(value1) if runtest == false
       # Check for test Pass / Fail
-      Report.testPassFail(test_pass, test_file_name, testStepIndex)
+      Report.testPassFail(testfunc, test_file_name, testStepIndex)
       Report.checkFailureThreshold(test_file_name)
 
     when 'execute_system_command'
       # print the test step information
       Report.printTestStepHeader(test_file_name, testStepIndex)
       # call the appropriate method
-      test_pass = WebFuncs.sys_command(value1) if runtest == false
+      testfunc = WebFuncs.sys_command(value1) if runtest == false
       # Check for test Pass / Fail
-      Report.testPassFail(test_pass, test_file_name, testStepIndex)
+      Report.testPassFail(testfunc, test_file_name, testStepIndex)
       Report.checkFailureThreshold(test_file_name)
 
     when 'check_box'
@@ -202,9 +202,9 @@ module TestSteps
       # print the test step information
       Report.printTestStepHeader(test_file_name, testStepIndex)
       # call the appropriate method
-      test_pass = WebFuncs.check_box(value1, locate1) if runtest == false
+      testfunc = WebFuncs.check_box(value1, locate1) if runtest == false
       # Check for test Pass / Fail
-      Report.testPassFail(test_pass, test_file_name, testStepIndex)
+      Report.testPassFail(testfunc, test_file_name, testStepIndex)
       Report.checkFailureThreshold(test_file_name)
 
     when 'radio_button'
@@ -213,46 +213,46 @@ module TestSteps
       Report.printTestStepHeader(test_file_name, testStepIndex)
       # call the appropriate method
       if runtest == false
-        test_pass = WebFuncs.radio_button(value1, value2, locate1, locate2)
+        testfunc = WebFuncs.radio_button(value1, value2, locate1, locate2)
       end
       # Check for test Pass / Fail
-      Report.testPassFail(test_pass, test_file_name, testStepIndex)
+      Report.testPassFail(testfunc, test_file_name, testStepIndex)
       Report.checkFailureThreshold(test_file_name)
 
     when 'browser_refresh'
       # print the test step information
       Report.printTestStepHeader(test_file_name, testStepIndex)
       # call the appropriate method
-      test_pass = WebFuncs.browser_refresh if runtest == false
+      testfunc = WebFuncs.browser_refresh if runtest == false
       # Check for test Pass / Fail
-      Report.testPassFail(test_pass, test_file_name, testStepIndex)
+      Report.testPassFail(testfunc, test_file_name, testStepIndex)
       Report.checkFailureThreshold(test_file_name)
 
     when 'browser_back'
       # print the test step information
       Report.printTestStepHeader(test_file_name, testStepIndex)
       # call the appropriate method
-      test_pass = WebFuncs.browser_back if runtest == false
+      testfunc = WebFuncs.browser_back if runtest == false
       # Check for test Pass / Fail
-      Report.testPassFail(test_pass, test_file_name, testStepIndex)
+      Report.testPassFail(testfunc, test_file_name, testStepIndex)
       Report.checkFailureThreshold(test_file_name)
 
     when 'browser_forward'
       # print the test step information
       Report.printTestStepHeader(test_file_name, testStepIndex)
       # call the appropriate method
-      test_pass = WebFuncs.browser_forward if runtest == false
+      testfunc = WebFuncs.browser_forward if runtest == false
       # Check for test Pass / Fail
-      Report.testPassFail(test_pass, test_file_name, testStepIndex)
+      Report.testPassFail(testfunc, test_file_name, testStepIndex)
       Report.checkFailureThreshold(test_file_name)
 
     when 'browser_quit'
       # print the test step information
       Report.printTestStepHeader(test_file_name, testStepIndex)
       # call the appropriate method
-      test_pass = WebFuncs.browser_quit if runtest == false
+      testfunc = WebFuncs.browser_quit if runtest == false
       # Check for test Pass / Fail
-      Report.testPassFail(test_pass, test_file_name, testStepIndex)
+      Report.testPassFail(testfunc, test_file_name, testStepIndex)
       Report.checkFailureThreshold(test_file_name)
 
     when 'insert_value_config'
@@ -260,37 +260,37 @@ module TestSteps
       Report.printTestStepHeader(test_file_name, testStepIndex)
       # call the appropriate method
       if runtest == false
-        test_pass = CustomFuncs.insert_value_config(value1, value2, locate1)
+        testfunc = CustomFuncs.insert_value_config(value1, value2, locate1)
       end
       # Check for test Pass / Fail
-      Report.testPassFail(test_pass, test_file_name, testStepIndex)
+      Report.testPassFail(testfunc, test_file_name, testStepIndex)
       Report.checkFailureThreshold(test_file_name)
 
     when 'open_portal_url'
       # print the test step information
       Report.printTestStepHeader(test_file_name, testStepIndex)
       # call the appropriate method
-      test_pass = CustomFuncs.open_portal_url(value1) if runtest == false
+      testfunc = CustomFuncs.open_portal_url(value1) if runtest == false
       # Check for test Pass / Fail
-      Report.testPassFail(test_pass, test_file_name, testStepIndex)
+      Report.testPassFail(testfunc, test_file_name, testStepIndex)
       Report.checkFailureThreshold(test_file_name)
 
     when 'handle_browser_window'
       # print the test step information
       Report.printTestStepHeader(test_file_name, testStepIndex)
       # call the appropriate method
-      test_pass = WebFuncs.handle_browser_window(value1) if runtest == false
+      testfunc = WebFuncs.handle_browser_window(value1) if runtest == false
       # Check for test Pass / Fail
-      Report.testPassFail(test_pass, test_file_name, testStepIndex)
+      Report.testPassFail(testfunc, test_file_name, testStepIndex)
       Report.checkFailureThreshold(test_file_name)
 
     when 'send_special_keys'
       # print the test step information
       Report.printTestStepHeader(test_file_name, testStepIndex)
       # call the appropriate method
-      test_pass = WebFuncs.send_special_keys(value1) if runtest == false
+      testfunc = WebFuncs.send_special_keys(value1) if runtest == false
       # Check for test Pass / Fail
-      Report.testPassFail(test_pass, test_file_name, testStepIndex)
+      Report.testPassFail(testfunc, test_file_name, testStepIndex)
       Report.checkFailureThreshold(test_file_name)
 
     else
