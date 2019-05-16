206a78830b24eac041f3091ed56296afa0a703f6
Aperrett <32389685+Aperrett@users.noreply.github.com>
2019-05-16 17:50:24 +0100

    bumped gem version and code tidy

--- Code/functions/handlers/write_box_data.rb
+++ Code/functions/handlers/write_box_data.rb
@@ -12,41 +12,23 @@ module TestSteps
         box = step_attributes[:testvalue]
         value = step_attributes[:testvalue2]
         locate = step_attributes[:locate]
-
         txt = ENV[value.to_s] || step_attributes[:testvalue2]
-        WriteBoxdata.input_value(box, txt, locate)
-      end
 
-      def self.input_value(box, txt, locate)
-        found_box = [
-          Browser.b.textarea("#{locate}": box).exist?,
-          Browser.b.text_field("#{locate}": box).exist?,
-          Browser.b.iframe("#{locate}": box).exist?
-        ]
+        elms = %i[textarea text_field iframe]
+
+        found_box = elms.map do |elm|
+          Browser.b.send(elm, "#{locate}": box).exists?
+        end.compact
 
         raise 'Multiple matches' if found_box.select { |i| i }.empty?
 
         index = found_box.index(true)
         return unless index
 
-        WriteBoxdata.write_to_box_data(index, box, txt, locate)
-      end
-
-      def self.write_to_box_data(index, box, txt, locate)
-        if index.zero?
-          Browser.b.textarea("#{locate}": box).wait_until(&:exists?).set txt
-          (Browser.b.textarea("#{locate}": box).text == txt)
-        elsif index == 1
-          Browser.b.text_field("#{locate}": box).wait_until(&:exists?).set txt
-          (Browser.b.text_field("#{locate}": box).text == txt)
-        elsif index == 2
-          Browser.b.iframe("#{locate}": box).wait_until(&:exists?).send_keys txt
-        end
+        Browser.b.send(elms[index], "#{locate}": box)
+               .wait_until(&:exists?).send_keys txt
         MyLog.log.info("Textbox: #{box} has correct value: #{txt}")
         true
-      rescue StandardError
-        MyLog.log.warn("Textbox: #{box} has the incorrect value: #{txt}")
-        false
       rescue StandardError
         MyLog.log.warn("Textbox: #{box} does not exist")
         false
